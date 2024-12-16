12167ab18bf714b2b797c14b1fd094c93fade314
Gadiel_Rod <infator7@gmail.com>
2024-12-16 17:53:25 -0500

    Creating HTTP Methods

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "../config/db.js";
+import Product from "../models/product.model.js";
 
 dotenv.config();
 
@@ -13,7 +14,27 @@ dotenv.config();
 const app = express();
 
 //Create a route
-app.post("/products", (req, res) => {});
+app.post("api/products", async (req, res) => {
+  const product = req.body; //user will send this data
+
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please Provide all fields" });
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.error("Error in saving Produc:", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
+//
 
 app.listen(5000, () => {
   connectDB();
