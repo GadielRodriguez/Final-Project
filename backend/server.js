0e8cb3b79551a0ef80986a1ac28ea2de0866ee19
Gadiel_Rod <infator7@gmail.com>
2024-12-20 12:42:20 -0500

    Making the code more modular, so we put the http methods on a diffrent file

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "../config/db.js";
 import Product from "../models/product.model.js";
+import mongoose from "mongoose";
 
 dotenv.config();
 
@@ -15,8 +16,20 @@ const app = express();
 
 app.use(express.json()); //Allows to accept json data in the req.body
 
+//Creating the Get Method
+
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    console.log("error in fetching products:", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
 //Create a route
-app.post("api/products", async (req, res) => {
+app.post("/api/products", async (req, res) => {
   const product = req.body; //user will send this data
 
   if (!product.name || !product.price || !product.image) {
@@ -36,7 +49,42 @@ app.post("api/products", async (req, res) => {
   }
 });
 
-//
+app.put("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const product = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res
+      .status(404)
+      .json({ success: false, message: "invalid Product Id" });
+  }
+
+  try {
+    const updateProduct = await Product.findByIdAndUpdate(id, product, {
+      new: true,
+    });
+    res.status(200).json({ success: true, data: updateProduct });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
+//The Delete Method is going to be dynamic, is going to get
+//the Id of the product we want to delete
+//What does the Async word does?
+
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params; //getting the ID
+  try {
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({ success: true, message: "Products deleted" });
+  } catch (error) {
+    //catching if we cant find the id we want to delete
+    console.log("Error in deleting product:", error.message);
+    res.status(404).json({ success: false, message: "Product not found" });
+  }
+});
 
 app.listen(5000, () => {
   connectDB();
