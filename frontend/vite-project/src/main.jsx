6d531ddd5754cd4aab342d64950ab0bbf05fb4d6
Gadiel_Rod <infator7@gmail.com>
2024-12-21 17:51:51 -0500

    Restarting again

--- frontend/vite-project/src/main.jsx
+++ frontend/vite-project/src/main.jsx
@@ -1,17 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-import App from "./App.jsx";
-//so fo the provide import i cant use "@"
-import { Provider } from "./components/ui/provider";
-import { BrowserRouter } from "react-router-dom";
-
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Provider>
-        <App />
-      </Provider>
-    </BrowserRouter>
-  </StrictMode>
-);
+    <App />
+  </StrictMode>,
+)
